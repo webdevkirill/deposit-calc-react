9d9a5f1ff0dcee117d6443e2e16222ff8a49b198
webdevkirill <kirill-fjodorov000@yandex.ru>
2021-04-19 21:08:33 +0300

    Добавляет стили выбора числа

--- src/common/typography/typography.js
+++ src/common/typography/typography.js
@@ -3,10 +3,10 @@ import { typographyStyles } from './typographyStyles';
 
 export const Header = ({ children, ...props }) => {
 	const styles = typographyStyles(props);
-	return <h3 className={styles.header}>children</h3>;
+	return <h3 className={styles.header}>{children}</h3>;
 };
 
 export const SubHeader = ({ children, ...props }) => {
 	const styles = typographyStyles(props);
-	return <h4 className={styles.subHeader}>children</h4>;
+	return <h4 className={styles.subHeader}>{children}</h4>;
 };
--- src/components/RangeSelector/rangeSelectorStyles.js
+++ src/components/RangeSelector/rangeSelectorStyles.js
@@ -4,31 +4,28 @@ import { withStyles } from '@material-ui/core/styles';
 export const PrettoSlider = withStyles({
 	root: {
 		color: '#B4D6EC',
-		height: 8,
-		padding: '6.5px 0',
+		height: 5,
+		padding: '8px 0',
 	},
 	thumb: {
 		height: 21,
 		width: 21,
 		backgroundColor: '#489DCF',
 		boxShadow: '0px 5px 10px rgba(136, 193, 227, 0.28)',
-		marginTop: -6.5,
+		marginTop: -8.5,
 		marginLeft: -10.5,
 		'&:focus, &:hover, &$active': {
 			boxShadow: '0px 5px 10px rgba(136, 193, 227, 0.28)',
 		},
 	},
 	active: {},
-	valueLabel: {
-		left: 'calc(-50% + 4px)',
-	},
 	track: {
-		height: 8,
+		height: 5,
 		borderRadius: 3,
 		backgroundColor: '#489DCF',
 	},
 	rail: {
-		height: 8,
+		height: 5,
 		borderRadius: 3,
 	},
 })(Slider);
