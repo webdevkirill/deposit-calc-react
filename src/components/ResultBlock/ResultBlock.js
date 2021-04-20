d22307b76e8465ac631c85d9bb96c03faa4ebc76
webdevkirill <kirill-fjodorov000@yandex.ru>
2021-04-20 22:47:53 +0300

    Добавляет рассчет значений выгоды пользователя

--- src/components/ResultBlock/ResultBlock.js
+++ src/components/ResultBlock/ResultBlock.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import ResultItem from '../ResultItem/ResultItem';
 import { resultBlockStyles } from './resultBlockStyles';
+import { useSelector } from 'react-redux';
 
 export default function ResultBlock() {
 	const styles = resultBlockStyles();
+	const { rate, depositeSumValue, monthValue } = useSelector(
+		(state) => state
+	);
+
+	const { summ, benefit } = calcBenefits(rate, depositeSumValue, monthValue);
+
 	const resultConfig = [
-		{ id: 'procent', label: 'Процентная ставка', value: '12,1%' },
+		{ id: 'procent', label: 'Процентная ставка', value: `${rate} %` },
 		{
 			id: 'sumAfterPeriod',
 			label: 'Сумма через',
-			value: '1 223 155,00 Р',
+			value: `${summ},00 Р`,
 			labelUnderlined: '24 мес',
 		},
-		{ id: 'money', label: 'Доход', value: '23 155,00 Р' },
+		{ id: 'money', label: 'Доход', value: `${benefit},00 Р` },
 	];
 	return (
 		<div className={styles.root}>
@@ -22,3 +29,16 @@ export default function ResultBlock() {
 		</div>
 	);
 }
+
+const calcBenefits = (rate, depositeSumValue, monthValue) => {
+	const monthСoeff = Math.floor(monthValue / 12) + (monthValue % 12) / 12;
+
+	const summ =
+		Math.floor((depositeSumValue * rate * monthСoeff) / 100) +
+		depositeSumValue;
+	const benefit = summ - depositeSumValue;
+	return {
+		summ,
+		benefit,
+	};
+};
